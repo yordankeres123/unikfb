03b4c25eb8890f0981e36a8912747c259e8adae9
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-05-06 21:31:04 +0700

    Add files via upload

---
+++ unik.sh
@@ -0,0 +1,40 @@
+clear
+echo "Cara Buat Akun Unik Lewat Termux"
+echo
+echo "Caranya Satu download dulu file fontnya"
+echo
+echo "https://shortid.co/zV4ZK"
+echo
+echo "salin link diatas dan paste ke google"
+echo
+echo "Tinggal dowload"
+echo
+echo "dua buka facebook kamu"
+echo
+read -p "Email/nohp : " emaill99
+
+if [ $emaill99 = $emaill99 ]
+then
+clear
+echo "Slamat Datang" $emaill99
+echo
+echo "Masukan kata Sandi"
+fi
+
+echo
+echo "Sandi facebook"
+echo
+read -p "Katasandi : " katalupa
+if [ $katalupa = $katalupa ]
+then
+clear
+echo "Slamat Datang" $emaill99
+echo
+fi
+
+echo "Setelah Itu Ganti nama seperti biasa"
+echo
+echo "salin font yang tadi didownload"
+echo
+echo "Masukan dinama Depan"
+
